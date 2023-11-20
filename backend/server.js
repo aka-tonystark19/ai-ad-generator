5ce2c5bfeb95829401f0741ae4cb8f1c4dd8451b
Aditya Phalod <adityaphalod@gmail.com>
2023-11-19 17:56:48 -0800

    ad ai pushed

---
+++ backend/server.js
@@ -0,0 +1,108 @@
+
+const { OpenAI } = require("openai");
+
+const express = require('express');
+const app = express();
+const port = 3000; 
+
+const openai = new OpenAI({
+  apiKey: 'YOUR-OPENAI-API-KEY',
+});
+
+
+const cors = require('cors')
+app.use(cors({origin:true}))
+
+app.use(express.json());
+
+app.post('/submit-form', async (req, res) => {
+  const formData = req.body;
+  console.log('Received form data:');
+  console.log(formData);
+
+  try {
+    const result = await generatePrompt(formData);
+    console.log(result);
+    res.status(200).json({ result: result });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+async function generatePrompt(formData) {
+  if (!openai.apiKey) {
+    throw new Error("OpenAI API key not configured, please follow instructions in README.md");
+  }
+
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: 'system',
+          content: generateAdString(formData)
+        },
+        {
+          role: 'user',
+          content: 'Generate ONE ad tag line for each ONLY, one from the perspective of a comedic outlook, and the other one as an infotainment. STRICTLY In the following format: Comedic: [comedicTagline], Infotainment: [infotainmentTagline]'
+        }
+      ],
+      temperature: 0.8,
+    });
+
+    const result = completion.choices[0].message.content;
+    return result;
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      throw new Error(JSON.stringify(error.response.data));
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      throw new Error('An error occurred during your request.');
+    }
+  }
+}
+
+
+function generateAdString(formData) {
+
+  const adString = `Imagine you are an experienced advertising agency. This is the brand info provided:
+  brandName: '${formData.brandName}',
+  serviceIndustry: '${formData.serviceIndustry}',
+  targetUserAge: [${formData.targetUserAge.map(age => "'" + age + "'").join(", ")}],
+  brandSlogan: '${formData.brandSlogan}',
+  additionalComments: '${formData.additionalComments}'`;
+
+  return adString;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+
+/*
+
+ChatGPT prompt:
+
+Imagine you are an advertising agency. This is the brand info provided:
+brandName: 'Bata',
+serviceIndustry: 'Sports',
+targetUserAge: [adults],
+brandSlogan: 'Boring corporate',
+additionalComments: 
+Generate ad tag lines, one from the perspective of a comedic outlook, and the other one as an infotainment. 
+
+
+Output:
+
+Comedic Tagline:
+"Step up your game with Bata: Because life's too short for boring shoes in a boring corporate world!"
+
+Infotainment Tagline:
+"Unleash Your Potential with Bata: Where Comfort Meets Performance for the Modern Athlete in the Boardroom."
+
+*/
--- src/App.js
+++ src/App.js
@@ -1,22 +1,162 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    brandName: '',
+    serviceIndustry: '',
+    targetUserAge: [],
+    brandSlogan: '',
+    additionalComments: '',
+  });
+
+  const [result, setResult] = useState('');
+
+
+  function extractPrompts(str) {
+    const prompts = str.split('\n');
+  
+    const comedic = prompts.find((prompt) => prompt.startsWith('Comedic:'));
+    const infotainment = prompts.find((prompt) => prompt.startsWith('Infotainment:'));
+  
+    const comedicPrompt = comedic ? comedic.replace('Comedic: ', '') : '';
+    const infotainmentPrompt = infotainment ? infotainment.replace('Infotainment: ', '') : '';
+  
+    return { comedicPrompt, infotainmentPrompt };
+  }
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+
+    if (type === 'checkbox') {
+      if (checked) {
+        setFormData({
+          ...formData,
+          targetUserAge: [...formData.targetUserAge, value],
+        });
+      } else {
+        setFormData({
+          ...formData,
+          targetUserAge: formData.targetUserAge.filter((age) => age !== value),
+        });
+      }
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/submit-form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+
+        if (data.result) {
+          setResult(data.result);
+        }
+        console.log('Data submitted successfully!');
+      } else {
+        console.error('Failed to submit data');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Ad AI</h1>
+        <div class="background-image"></div>
+        <form onSubmit={handleSubmit} className="questionnaire">
+          <label>
+            Brand Name:
+            <input
+              type="text"
+              name="brandName"
+              value={formData.brandName}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Service Industry:
+            <input
+              type="text"
+              name="serviceIndustry"
+              value={formData.serviceIndustry}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Target User Age:
+            <div className="checkbox-options">
+              {['Kids', 'Teens', 'Young Adults', 'Middle-Aged', 'Old-Aged'].map(age => (
+                <label key={age} className="checkbox-label">
+                  <input
+                    type="checkbox"
+                    name="targetUserAge"
+                    value={age}
+                    checked={formData.targetUserAge.includes(age)}
+                    onChange={handleChange}
+                  />
+                  {age}
+                </label>
+              ))}
+            </div>
+          </label>
+
+          <label>
+            Brand Slogan:
+            <input
+              type="text"
+              name="brandSlogan"
+              value={formData.brandSlogan}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Additional Comments:
+            <textarea
+              name="additionalComments"
+              value={formData.additionalComments}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit">Generate Ads</button>
+        </form>
+
+    
+        {result && (
+  <div className="result-container">
+    <table className="prompt-table">
+      <tr>
+        {/* <th>Comedic Ad</th> */}
+        <th>Infotainment Ad</th>
+      </tr>
+      <tr>
+        {/* <td>{extractPrompts(result).comedicPrompt}</td> */}
+        <td>{extractPrompts(result).infotainmentPrompt}</td>
+      </tr>
+    </table>
+  </div>
+)}
+
       </header>
     </div>
   );
